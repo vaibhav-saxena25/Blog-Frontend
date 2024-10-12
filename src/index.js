cd7353d93f7498e7cbb1f365412d2d31a3ab3027
vaibhav-saxena25 <149486002+vaibhav-saxena25@users.noreply.github.com>
2024-10-12 21:58:33 +0530

    Update index.js
    done

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {Provider} from 'react-redux'
 import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-export const server = 'https://blog-app-backend-gkdv.onrender.com';
+export const server = 'http://localhost:8080';
 root.render(
   <Provider store = {store}>
   <BrowserRouter>
