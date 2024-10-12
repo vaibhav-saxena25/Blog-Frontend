021c67c95895b6ce87e9eb63747db3aeca1cc26b
vaibhav-saxena25 <149486002+vaibhav-saxena25@users.noreply.github.com>
2024-10-12 21:59:20 +0530

    Update Blog.js
    done

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,37 +1,44 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import BlogCard from '../components/BlogCard'
-import { server } from '../index'
+import BlogCard from '../components/BlogCard';
+import { server } from '../index';
 
 const Blog = () => {
-  const [blogs, setBlogs] = useState([])
-  const getAllBlogs = async(req,res)=>{
-    try{
-      const {data} = await axios.get(`${server}/api/v1/blog/all-blogs`)
+  const [blogs, setBlogs] = useState([]);
 
+  const getAllBlogs = async () => {
+    try {
+      const { data } = await axios.get(`${server}/api/v1/blog/all-blogs`);
       console.log(data);
-    
-      if(data?.success){
+
+      if (data?.success) {
         setBlogs(data?.blogs);
       }
-    }catch(err){
+    } catch (err) {
       console.log(err);
     }
-  }
-  useEffect(()=>{
+  };
+
+  useEffect(() => {
     getAllBlogs();
-  },[]);
+  }, []);
+
   return (
     <div>
-      {blogs && blogs.map(blog=><BlogCard id = {blog?._id} 
-      isUser = {localStorage.getItem('userId')=== blog?.user._id} 
-      title = {blog?.title} 
-      description = {blog?.description} 
-      image = {blog?.image} 
-      username = {blog?.user.username} 
-      time={blog?.createdAt}/>)}
+      {blogs && blogs.map((blog) => (
+        <BlogCard
+          key={blog?._id} // Ensure there's a key for each mapped item
+          id={blog?._id}
+          isUser={localStorage.getItem('userId') === blog?.user?._id} // Add null-check for blog.user
+          title={blog?.title}
+          description={blog?.description}
+          image={blog?.image}
+          username={blog?.user?.username} // Add null-check for blog.user
+          time={blog?.createdAt}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
